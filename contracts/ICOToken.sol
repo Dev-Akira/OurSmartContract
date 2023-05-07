63d3d6598d02553310e4f0a481c9fb77d74a23ae
Dev-Akira <janisflores360@gmail.com>
2023-05-07 03:29:28 -0400

    test version

---
+++ contracts/ICOToken.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+ contract ICOToken {
+    address public owner;
+    string public name = "ICO";
+    string public symbol = "ICO";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+    
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply;
+        owner = msg.sender;
+    }
+    
+    function mint(uint256 _amount) public onlyOwner {
+        balanceOf[owner] += _amount;
+        totalSupply += _amount;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+    
+    function transfer(address _recipient, uint256 _amount) public returns (bool success) {
+        require(_recipient != address(0), "Invalid recipient address");
+        require(balanceOf[msg.sender] >= _amount, "Insufficient balance");
+        balanceOf[msg.sender] -= _amount;
+        balanceOf[_recipient] += _amount;
+        emit Transfer(msg.sender, _recipient, _amount);
+        return true;
+    }
+    
+    function approve(address _spender, uint256 _amount) public returns (bool success) {
+        require(_spender != address(0), "Invalid spender address");
+        allowance[msg.sender][_spender] = _amount;
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+    
+    function transferFrom(address _sender, address _recipient, uint256 _amount) public returns (bool success) {
+        require(_sender != address(0), "Invalid sender address");
+        require(_recipient != address(0), "Invalid recipient address");
+        require(balanceOf[_sender] >= _amount, "Insufficient balance");
+        require(allowance[_sender][msg.sender] >= _amount, "Insufficient allowance");
+        balanceOf[_sender] -= _amount;
+        balanceOf[_recipient] += _amount;
+        allowance[_sender][msg.sender] -= _amount;
+        emit Transfer(_sender, _recipient, _amount);
+        return true;
+    }
+    
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
